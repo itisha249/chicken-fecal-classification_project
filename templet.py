70ed663e21d45e080a5acb7962f9c18b964c668c
Itisha Patel <ipatel@dc.nexuslink.com>
2024-11-11 17:04:23 +0530

    folder structure added

--- templet.py
+++ templet.py
@@ -21,18 +21,24 @@ list_of_files = [
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
 ]
 
 for filepath in list_of_files:
-    filepath = path(filepath)
+    filepath = Path(filepath)
     filedir,filename = os.path.split(filepath)
 
     if filedir != "":
         os.makedirs(filedir,exist_ok=True)
-        logging.info(f"creating directory;(filedir) for the file:(filename)"):
+        logging.info(f"creating directory;(filedir) for the file:(filename)")
 
 
     if  (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already exist")
+
 
